4b46d55f081a3d18c61bce9a9661d68e61c1a99c
Lucas Rodrigues Farias <48932792+Pretinho10@users.noreply.github.com>
2022-04-01 22:08:11 -0300

    Create teste_calculadora.py

---
+++ teste_calculadora.py
@@ -0,0 +1,6 @@
+
+numero1 = input("Digite o primeiro número: ")
+numero2 = input("Digite o segundo número: ")
+
+resultado = int(numero1) + int(numero2)
+print("O resultado da operação é: " + str(resultado))
